17a118f1916f9f6d76af811a0b0594f507414bfc
jonandthink <jonandthink@gmail.com>
2016-01-02 19:08:58 +0700

    Second Commit

---
+++ assets/js/effect.js
@@ -0,0 +1,14 @@
+// function sticky_relocate() {
+//     var window_top = $(window).scrollTop();
+//     var div_top = $('#at-top-anchor').offset().top;
+//     if (window_top > div_top) {
+//         $('#at-top').addClass('stick');
+//     } else {
+//         $('#at-top').removeClass('stick');
+//     }
+// }
+
+// $(function () {
+//     $(window).scroll(sticky_relocate);
+//     sticky_relocate();
+// });
